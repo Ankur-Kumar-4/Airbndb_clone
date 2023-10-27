78feb2ff04bb26b659a83460394304d9daa6ecfe
Ankurdyno <98178905+Ankurdyno@users.noreply.github.com>
2023-10-27 17:05:34 +0530

    v-5

--- search.js
+++ search.js
@@ -1,32 +1,3 @@
-// const url =
-//   "https://airbnb13.p.rapidapi.com/search-location?location=Paris&checkin=2023-11-16&checkout=2023-11-17&adults=1&children=0&infants=0&pets=0&page=1&currency=USD";
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Key": "b12e290677mshb57cd20d3c60400p12c2e1jsnd85322ab66e5",
-//     "X-RapidAPI-Host": "airbnb13.p.rapidapi.com",
-//   },
-// };
-// let ekhibar = 0;
-// console.log("main hun zinda shayad");
-// try {
-//   async function callAirBnb() {
-//     ekhibar = 1;
-//     console.log("ooh la la le lo, oo laa le lo");
-//     const response = await fetch(url, options);
-//     const result = await response.text();
-//     console.log(result);
-//   }
-//   if (ekhibar == 0) {
-//     console.log("main hun zinda shayad");
-//     callAirBnb();
-//   }
-// } catch (error) {
-//   console.error(error);
-// }
-
-
-
 // Retrieve the JSON string from local storage
 const jsonString = localStorage.getItem("inputValues");
 
@@ -36,8 +7,100 @@ const myObject = JSON.parse(jsonString);
 // Now, myObject contains the object that was saved in local storage
 console.log(myObject);
 
-const placeName = document.getElementById("placeName").innerHTML = myObject.location;
+let location = myObject.location;
+let checkIn = myObject.checkIn;
+let checkOut = myObject.checkOut;
+let adults = myObject.adults;
+let children = myObject.children;
+let infants = myObject.infants;
+let pets = myObject.pets;
 
-const duration = document.getElementById("duration").innerHTML = myObject.checkIn;
+var data = {};
 
-const guestsNo = document.getElementById("guestsNo").innerHTML = parseInt(myObject.children) +parseInt(myObject.infants) + parseInt(myObject.pets)+parseInt(myObject.adults);
+const url = `https://airbnb13.p.rapidapi.com/search-location?location=${location}&checkin=${checkIn}&checkout=${checkOut}&adults=${adults}&children=${children}&infants=${infants}&pets=${pets}&page=1&currency=USD`;
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b12e290677mshb57cd20d3c60400p12c2e1jsnd85322ab66e5",
+    "X-RapidAPI-Host": "airbnb13.p.rapidapi.com",
+  },
+};
+let ekhibar = 0;
+
+try {
+  async function callAirBnb() {
+    ekhibar = 1;
+    console.log("ooh la la le lo, oo laa le lo");
+    const response = await fetch(url, options);
+    const result = await response.json();
+    data = result;
+    renderCount(data);
+    console.log(result);
+  }
+  if (ekhibar == 0) {
+    console.log("main hun zinda shayad");
+    callAirBnb();
+  }
+} catch (error) {
+  console.error(error);
+}
+
+const placeName = (document.getElementById("placeName").innerHTML = location);
+
+const duration = (document.getElementById("duration").innerHTML = checkIn);
+
+const guestsNo = (document.getElementById("guestsNo").innerHTML =
+  parseInt(adults) + parseInt(children) + parseInt(infants) + parseInt(pets));
+
+const listingContainer = document.getElementById("listingContainer");
+
+function displayListing(i) {
+  const hr = document.createElement("hr");
+  const listing = document.createElement("div");
+  listing.className = "listing_item";
+
+  listing.innerHTML = `
+        
+    <img class="listing_image" src="Image.png" alt="" />
+
+            <div class="listing_info">
+              <div>
+                <div class="detail">Entire home in ${data.result[i].city}</div>
+
+                <div class="plotName">${data.result[i].name}</div>
+              </div>
+
+              <div class="detail">
+                4-6 guests · Entire Home · ${data.result[i].bedrooms} beds · ${data.result[i].bathrooms} bath<br />
+                Wifi · Kitchen · Free Parking
+              </div>
+
+              <div class="list_footer">
+                <div>
+                  ${data.result[i].rating} <img src="${data.result[i].images[0]}" alt="" />
+                  <span class="footerInfo"> (${data.result[i].reviewsCount} reviews)</span>
+                </div>
+
+                <div class="price">
+                  $${data.result[i].price.rate} <span class="footerInfo">/night</span>
+                </div>
+              </div>
+            </div>
+    
+    
+    `;
+
+  listingContainer.appendChild(listing);
+  listingContainer.appendChild(hr);
+}
+
+function renderCount(data) {
+  if (!data) {
+    console.log("No data");
+    return;
+  }
+  console.log("there is data", data);
+  for (let i = 0; i < data.result?.length; i++) {
+    displayListing(i);
+  }
+}
